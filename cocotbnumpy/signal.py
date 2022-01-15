f879723f4ec15debf4e846ef855692866dc8af83
Justin Hugon <jhugon@users.noreply.github.com>
2022-01-15 15:36:48 -0700

    Added test_signal.py

--- cocotbnumpy/signal.py
+++ cocotbnumpy/signal.py
@@ -14,12 +14,21 @@ class NumpySignal:
 
         npsigdc = NumpySignal([0,0,1,1,1],dontcaremask=[1,1])
 
+        The mask is only taken into account on the first member of an equality
+        check, that is:
+
+        in1 = [0,1,0,1,0,1]
+        in2 = [1,0,0,1,0,1]
+        s1 = NumpySignal(in1,[1,1])
+        s2 = NumpySignal(in2)
+        assert s1 == s2
+        assert s2 != s1
+
         """
         waveform = np.array(waveform)
-        try:
-            dontcaremask = np.array(dontcaremask)
-        except ValueError:
-            dontcaremask = np.zeros(waveform.size)
+        if dontcaremask is None:
+            dontcaremask = np.zeros(waveform.size, dtype=np.uint8)
+        dontcaremask = np.array(dontcaremask)
 
         if len(waveform) > len(dontcaremask):
             tmp = np.zeros(waveform.size)
@@ -36,10 +45,17 @@ class NumpySignal:
         result = ""
         for x, m in zip(self.waveform, self.dontcaremask):
             if m:
-                result += "{:" + str(width) + "}".format("-")
+                result += ("{:" + str(width) + "} ").format("-")
             else:
-                result += "{:" + str(width) + "}".format(x)
+                result += ("{:" + str(width) + "} ").format(x)
+        if len(result) > 0:
+            result = result[:-1]
         return result
+
+    def __repr__(self):
+        return (
+            f"NumpySignal({list(self.waveform)},dontcaremask={list(self.dontcaremask)})"
+        )
 
     def get_max_str_width(self):
         l = list(self.waveform)
@@ -51,7 +67,18 @@ class NumpySignal:
         return len(self.waveform)
 
     def __eq__(self, other):
+        """
+        Only use the dontcaremask of self, not other.
+        This means in a comparison `a == b` that a.dontcaremask will be used.
+        """
         try:
-            return self.waveform == other.waveform
+            equal_elements = np.equal(self.waveform, other.waveform)
+            result = np.logical_or(equal_elements, self.dontcaremask)
+            return result.all()
         except AttributeError:
             return False
+        except ValueError:
+            return False
+
+    def __getitem__(self, i):
+        return self.waveform[i]
---
+++ tests/test_signal.py
@@ -0,0 +1,39 @@
+from cocotbnumpy.signal import NumpySignal
+
+
+def test_NumpySignal_basics():
+    in1 = [0, 1, 0, 1, 0, 1]
+    s1 = NumpySignal(in1)
+    assert str(s1) == str(in1).replace(",", "")[1:-1]
+    assert s1.get_max_str_width() == 1
+    assert len(s1) == len(in1)
+    assert s1 == NumpySignal(in1)
+    for i in range(len(in1)):
+        assert s1[i] == in1[i]
+
+    in2 = list(range(10))
+    s2 = NumpySignal(in2)
+    assert str(s2) == str(in2).replace(",", "")[1:-1]
+    assert s2.get_max_str_width() == 1
+    assert len(s2) == len(in2)
+    assert (s2 == NumpySignal(in2)).all()
+    for i in range(len(in2)):
+        assert s2[i] == in2[i]
+
+    assert s1[0] == s2[0]
+    assert s1[5] != s2[5]
+    assert s1 != s2
+
+
+def test_NumpySignal_masks():
+    in1 = [0, 1, 0, 1, 0, 1]
+    in2 = [1, 0, 0, 1, 0, 1]
+    s1 = NumpySignal(in1, [1, 1])
+    s2 = NumpySignal(in2)
+    for i in range(len(in1)):
+        if i < 2:
+            assert s1[i] != in2[i]
+        else:
+            assert s1[i] == in2[i]
+    assert s1 == s2
+    assert s2 != s1
